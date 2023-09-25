284a79c5f9122101ac1eb37e86d52723bd24a35c
Plamedi Pindi <plamedipindi77@gmail.com>
2023-09-25 15:49:00 +0100

    Atualiando a home

--- resources/views/layouts/_includes/site/Menu.blade.php
+++ resources/views/layouts/_includes/site/Menu.blade.php
@@ -1,147 +1,114 @@
-<!--HEADER-->
-<header>
-    <div id="lgx-header" class="lgx-header">
-        <div class="header-top">
-            <div class="header-top-inner">
-                <div class="lgx-container-fluid"> <!--lgx-container-fluid-->
-                    <div class="row">
-                        <div class="col-sm-6">
-                            <div class="contact">
-                                <ul class="list-inline">
-                                    <li><span class="question-text">Order Now?</span> <i class="fa fa-phone" aria-hidden="true"></i> +123 125 145 </li>
-                                    <li><i class="fa fa-envelope" aria-hidden="true"></i> <a href="https://themearth.com/cdn-cgi/l/email-protection" class="__cf_email__" data-cfemail="84f1f7e1f6eae5e9e1c4e0ebe9e5edeaaae7ebe9">[email&#160;protected]</a></li>
-                                </ul>
-                            </div>
+<!-- Page Preloder -->
+<div id="preloder">
+    <div class="loader"></div>
+</div>
+
+<!-- Humberger Begin -->
+<div class="humberger__menu__overlay"></div>
+<div class="humberger__menu__wrapper">
+    <div class="humberger__menu__logo">
+        <a href="{{ route('home') }}"><img src="/site/img/icon.png" alt=""><h2>Su<span>m</span>ba</h2></a>
+    </div>
+    <div class="humberger__menu__cart">
+        <ul>
+
+            <li><a href="{{ route('shop.shoppingCart') }}"><i class="fa fa-shopping-bag"></i>
+                    <span>{{ \Cart::getContent()->count() }}</span></a></li>
+        </ul>
+        <div class="header__cart__price">item: <span>{{ number_format(\Cart::getTotal(), 2, ',', '.') }} KZ</span>
+        </div>
+    </div>
+    <div class="humberger__menu__widget">
+
+        <div class="header__top__right__auth">
+            <a href="{{ route('login') }}"><i class="fa fa-user"></i> Login</a>
+        </div>
+    </div>
+    <nav class="humberger__menu__nav mobile-menu">
+        <ul>
+            <li class="active"><a href="{{ route('home') }}">Home</a></li>
+            <li><a href="r{{ route('shop.shop-grid') }}">Loja</a></li>
+            <li><a href="{{ route('site.contatos') }}">Contatatos</a></li>
+        </ul>
+    </nav>
+    <div id="mobile-menu-wrap"></div>
+    <div class="header__top__right__social">
+        <a href="#"><i class="fa fa-facebook"></i></a>
+        <a href="#"><i class="fa fa-instagram"></i></a>
+    </div>
+
+</div>
+<!-- Humberger End -->
+
+<!-- Header Section Begin -->
+<header class="header">
+    <div class="header__top">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-4">
+
+                </div>
+                <div class="col-lg-8">
+                    <div class="header__top__right">
+                        <div class="header__top__right__social">
+                            <a href="#"><i class="fa fa-facebook"></i></a>
+                            <a href="#"><i class="fa fa-instagram"></i></a>
+
                         </div>
-                        <div class="col-sm-6">
-                            <div class="right-menu">
-                                <ul class="list-inline">
-                                    <li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
-                                    <li><a href="#"><i class="fa fa-facebook-f" aria-hidden="true"></i></a></li>
-                                    <li><a href="#"><i class="fa fa-pinterest-p" aria-hidden="true"></i></a></li>
-                                    <li><a href="#"><i class="fa fa-google" aria-hidden="true"></i></a></li>
-                                    <li><a href="#"><i class="fa fa-youtube-play" aria-hidden="true"></i></a></li>
-                                    <li><a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
-                                    <li><a href="#"><i class="fa fa-vine" aria-hidden="true"></i></a></li>
-                                </ul>
+                        @guest
+                            <div class="header__top__right__auth">
+                                <a href="{{ route('login') }}"><i class="fa fa-user"></i> Login</a>
                             </div>
-                        </div>
+                        @endguest
+                        @auth
+                        <div class="dropdown">
+
+                            <button class="btn  dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                              Minha conta
+                            </button>
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="{{ route('Admindashboard') }}">Painel de controlo</a></li>
+                                <li><a class="dropdown-item" href="{{ route('logout') }}"></i> Sair   </a></li>
+                            </ul>
+                          </div>
+
+                        @endauth
                     </div>
                 </div>
             </div>
-        </div> <!--//.header-top-->
-
-
-
-        <div class="lgx-header-position lgx-header-position-fixed lgx-header-position-white"> <!--lgx-header-position-fixed lgx-header-position-white lgx-header-fixed-container lgx-header-fixed-container-gap lgx-header-position-white-->
-            <div class="lgx-container-fluid"> <!--lgx-container-fluid-->
-                <nav class="navbar navbar-default lgx-navbar">
-                    <div class="navbar-header">
-                        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-                            <span class="sr-only">Toggle navigation</span>
-                            <span class="icon-bar"></span>
-                            <span class="icon-bar"></span>
-                            <span class="icon-bar"></span>
-                        </button>
-                        <div class="lgx-logo"> <!--lgx-logo-default lgx-logo-fixed lgx-logo-brand lgx-logo-black-->
-                            <a href="index4.html" class="lgx-scroll">
-                                <img src="/site/img/logo2.png" alt="Restaura Logo"/>
-                            </a>
-                        </div>
-                    </div>
-                    <div id="navbar" class="navbar-collapse collapse lgx-collapse">
-                        <ul class="nav navbar-nav lgx-nav">
-                            <li>
-                                <a href="index.html" class="dropdown-toggle active" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Home <span class="caret"></span></a>
-                                <ul class="dropdown-menu multi-level">
-                                    <li><a href="index.html">Home (Default)</a></li>
-                                    <li><a href="index2.html">Home Two</a></li>
-                                    <li><a href="index3.html">Home Three</a></li>
-                                    <li><a href="index4.html">Home Four</a></li>
-                                    <li><a href="index5.html">Home Five</a></li>
-                                    <li><a href="index6.html">Home Six</a></li>
-                                    <li><a href="index7.html">Home Seven</a></li>
-                                    <li><a href="index8.html">Home Eight</a></li>
-                                    <li><a href="index9.html">Home Nine</a></li>
-                                    <li><a href="index10.html">Home Ten</a></li>
-                                    <li><a href="index11.html">Home Eleven</a></li>
-                                    <li><a href="index12.html">Home Twelve</a></li>
-                                    <li><a href="index13.html">Home Thirteen</a></li>
-                                </ul>
-                            <li>
-                            <li>
-                                <a href="index.html" class="dropdown-toggle active" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Pages <span class="caret"></span></a>
-                                <ul class="dropdown-menu multi-level">
-                                    <li><a href="about.html">About</a></li>
-                                    <li><a href="service.html">Services</a></li>
-                                    <li class="dropdown-submenu">
-                                        <a href="#" class="dropdown-toggle active" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Food Menu<span class="caret"></span></a>
-                                        <ul class="dropdown-menu">
-                                            <li><a href="menuitems.html">Menu Items</a></li>
-                                            <li><a href="menuitem.html">Single Menu Items</a></li>
-                                        </ul>
-                                    </li>
-                                    <li><a href="reservation.html">Reservation</a></li>
-                                    <li class="dropdown-submenu">
-                                        <a href="#" class="dropdown-toggle active" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Our Chefs<span class="caret"></span></a>
-                                        <ul class="dropdown-menu">
-                                            <li><a href="chefs.html">Chefs</a></li>
-                                            <li><a href="chef.html">Single Chef</a></li>
-                                        </ul>
-                                    </li>
-                                    <li class="dropdown-submenu">
-                                        <a href="#" class="dropdown-toggle active" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">News <span class="caret"></span></a>
-                                        <ul class="dropdown-menu">
-                                            <li><a href="news.html">News</a></li>
-                                            <li><a href="news-single.html">News Single</a></li>
-                                        </ul>
-                                    </li>
-                                    <li><a href="speacial.html">Special Items</a></li>
-                                    <li><a href="contact.html">Contact</a></li>
-                                    <li class="dropdown-submenu">
-                                        <a href="#" class="dropdown-toggle active" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
-                                        <ul class="dropdown-menu">
-                                            <li class="dropdown-submenu">
-                                                <a href="#" class="dropdown-toggle active" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown Two<span class="caret"></span></a>
-                                                <ul class="dropdown-menu">
-                                                    <li><a href="#">Dropdown Three</a></li>
-                                                    <li><a href="#">Dropdown Three</a></li>
-                                                </ul>
-                                            </li>
-                                            <li><a href="#">Dropdown Two</a></li>
-                                            <li><a href="#">Dropdown Two</a></li>
-                                        </ul>
-                                    </li>
-                                </ul>
-                            </li>
-                            <li><a class="lgx-scroll active" href="#lgx-about">About</a></li>
-                            <li><a class="lgx-scroll" href="#lgx-menu">Menu</a></li>
-                            <li><a class="lgx-scroll" href="#lgx-speacial">Speacial</a></li>
-                            <li><a class="lgx-scroll" href="#lgx-news">News</a></li>
-                            <li><a class="lgx-scroll btn lgx-btn" href="#lgx-reservation"><span>Reservation</span></a></li>
-                        </ul>
-                        <div class="lgx-nav-right">
-                            <div class="lgx-search">
-                                <a href="#"><i class="fa fa-search"></i></a>
-                                <div class="search">
-                                    <form action="#" method="GET" role="search">
-                                        <input type="text" class="form-control input" name="q" placeholder="Search for snippets and hit enter">
-                                    </form>
-                                    <div class="close"><i class="fa fa-close"></i></div>
-                                </div>
-                            </div>
-                            <div class="lgx-cart-area">
-                                <a class="lgx-cart" href="#">
-                                    <img src="/site/img/cart-icon.png" alt="cart">
-                                    <span class="notification">2</span>
-                                </a>
-                            </div>
-                        </div>
-                    </div><!--/.nav-collapse -->
+        </div>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-4">
+                <div class="header__logo">
+                    <a href="{{ route('home') }}"><img src="/site/img/icon.png" alt=""><h2>Su<span>m</span>ba</h2></a>
+                </div>
+            </div>
+            <div class="col-lg-5">
+                <nav class="header__menu">
+                    <ul>
+                        <li class="active"><a href="{{ route('home') }}">Home</a></li>
+                        <li ><a href="{{ route('shop.shop-grid') }}">Loja</a></li>
+                        <li><a href="{{ route('site.contatos') }}">Contatatos</a></li>
+                    </ul>
                 </nav>
             </div>
-            <!-- //.CONTAINER -->
+            <div class="col-lg-3">
+                <div class="header__cart">
+                    <ul>
+
+                        <li><a href="{{ route('shop.shoppingCart') }}"><i class="fa fa-shopping-bag"></i>
+                                <span>{{ \Cart::getContent()->count() }}</span></a></li>
+                    </ul>
+                    <div class="header__cart__price">item:
+                        <span>{{ number_format(\Cart::getTotal(), 2, ',', '.') }} KZ</span></div>
+                </div>
+            </div>
+        </div>
+        <div class="humberger__open">
+            <i class="fa fa-bars"></i>
         </div>
     </div>
 </header>
-<!--HEADER END-->
+<!-- Header Section End -->
